1b6ae7f0c1fc3f6f5771a61f25ded346c44ff926
App Academy Student <student@appacademy.io>
2019-09-23 17:50:14 -0700

    work on associations

---
+++ W6D1/polls_app/app/models/poll.rb
@@ -0,0 +1,10 @@
+class Poll < ApplicationRecord
+  belongs_to :author,
+    foreign_key: :author_id,
+    class_name: :User
+
+  has_many :questions,
+    foreign_key: :poll_id,
+    class_name: :Question
+
+end
